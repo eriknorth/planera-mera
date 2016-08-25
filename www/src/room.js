52d6936072e3b5cd996af83893ffcd4175e7dcce
Eriks Zaharans <erikszah@gmail.com>
2016-08-25 23:02:25 +0200

    Commented out the testing part
    Former-commit-id: e29117821bc89ea5f85995d72fec42d9c41c5fe7

--- www/src/room.js
+++ www/src/room.js
@@ -1266,12 +1266,12 @@ GameObj.Room.prototype = {
 			else {
 				
 				// TODO: [LISA] Force a task. Uncoment these three lines below and comment the one below text "Save task in game object"
-				var task = res.rows.item(0);
-				task.task = 327;
-				GameObj.task = task;
+				// var task = res.rows.item(0);
+				// task.task = 327;
+				// GameObj.task = task;
 				
 				// Save task in game object
-				// GameObj.task = res.rows.item(0);
+				GameObj.task = res.rows.item(0);
 				
 				// Load task in the object
 				self._currTask = self._taskArray.filter(function (obj) {
